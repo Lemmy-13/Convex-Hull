1bca15a08a041a8faf5bd83ed5a66ad499e6aebd
Lemmy-13 <Lemuel_alvarez@uri.edu>
2022-04-07 15:49:04 -0400

    Update main.cpp

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -1,118 +1,262 @@
-#include <iostream>
-#include <vector>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 
-typedef long long ll;
-struct Point {
-    ll x, y;
-    Point(ll a, ll b) :x(a), y(b) {};
-    Point() {};
-    bool operator<(const Point &rhs) const {
-        if (x != rhs.x) return x < rhs.x;
-        return y < rhs.y;
+int orientare(pair<int, int> p, pair<int, int> q, pair<int, int> r)
+{
+    int val = (q.second - p.second) * (r.first - q.first)
+              - (q.first - p.first) * (r.second - q.second);
+    if (val == 0)
+    {
+        return 0;
     }
-};
-
-std::vector<Point> point;
-
-ll ccw(Point pt1, Point pt2, Point pt3) {
-    ll ret = pt1.x*pt2.y + pt2.x*pt3.y + pt3.x*pt1.y;
-    ret -= (pt2.x*pt1.y + pt3.x*pt2.y + pt1.x*pt3.y);
-    return ret;
-}
-ll dist(Point pt1, Point pt2) {
-    ll dx = pt2.x - pt1.x;
-    ll dy = pt2.y - pt1.y;
-    return dx * dx + dy * dy;
+    return (val > 0) ? 2 : 1; // 2 left
 }
 
+bool cmp(pair<int, int> p, pair<int, int> q){
+    if(p.first < q.first)
+        return 1;
+    else if(p.first == q.first)
+        return p.second < q.second;
+    else
+        return 0;
+}
 
 int main()
 {
-    //Creates the application window and adds the title.
-    sf::RenderWindow window(sf::VideoMode(1400, 1050), "Convex Hull");
 
-    //Check
-    //bool valid = false;
+    int n=0;
+    int k = 0, k2=0;
+    int t=0;
+    int ii=2;
+    int j = 0;
+    int careLinie = 0;
+    int careLinie2 = 0;
 
-    while (window.isOpen())
+    int x, y;
+    vector<pair<int, int> > a;
+
+
+    sf::RenderWindow app(sf::VideoMode(1400, 1050), "Press ESCAPE/ENTER to continue");
+
+
+    while (app.isOpen())
     {
+        // Process events
         sf::Event event;
-        while (window.pollEvent(event))
+        while (app.pollEvent(event))
         {
+            // Close window : exit
             if (event.type == sf::Event::Closed)
-                window.close();
+                app.close();
 
-            //Adds Mouse.
-            else if (event.type == sf::Event::MouseButtonPressed &&
-                     event.mouseButton.button == sf::Mouse::Left)
+            if (event.type == sf::Event::KeyPressed)
             {
-                //Gets the x & y coordinates.
-                float x = event.mouseButton.x;
-                float y = event.mouseButton.y;
-
-                Point p(x, y);
-
-                    point.push_back(p);
-
-                std::vector<Point> hull;
-                std::swap(point[0], *min_element(point.begin(), point.end()));
-                sort(point.begin() + 1, point.end(), [](Point x, Point y) {
-                    ll cw = ccw(point[0], x, y);
-                    if (cw == 0) return dist(point[0], x) < dist(point[0], y);
-                    return cw > 0;
-                });
-
+                if (event.key.code == sf::Keyboard::Escape || event.key.code == sf::Keyboard::Enter)
+                {
+                    app.close();
+                }
 
             }
 
-                //Adds Keyboard
-            else if (event.type == sf::Event::KeyPressed)
+            if (event.type == sf::Event::MouseButtonPressed)
             {
-                //Space bar to stop and compute.
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+                if (event.mouseButton.button == sf::Mouse::Left)
                 {
-                    //valid = true;
+                    a.push_back(make_pair(event.mouseButton.x, event.mouseButton.y));
+                    n++;
+                }
+            }
+
+        }
+
+        // Clear screen
+        app.clear();
+
+        for(int i=0; i<n; i++){
+            sf::CircleShape cerc3(3);
+            cerc3.setPosition(a[i].first , a[i].second);
+            app.draw(cerc3);
+        }
+
+        app.display();
+    }
+
+    sort(a.begin(), a.end(), cmp);
+
+    int fs_i = n-3;
+    vector<pair<int, int> > fi;
+    fi.push_back(a[0]);
+    fi.push_back(a[1]);
+
+    vector<pair<int, int> > fs;
+    fs.push_back(a[n-1]);
+    fs.push_back(a[n-2]);
+
+    sf::Vertex linii[n][2];
+    sf::Vertex linii2[n][2];
+
+    sf::Vertex line[]{
+            sf::Vertex(sf::Vector2f(fi[0].first, fi[0].second)),
+            sf::Vertex(sf::Vector2f(fi[1].first, fi[1].second))
+    };
+    linii[careLinie][0] = line[0];
+    linii[careLinie++][1] = line[1];
+
+    sf::Vertex line2[]{
+            sf::Vertex(sf::Vector2f(fs[0].first, fs[0].second)),
+            sf::Vertex(sf::Vector2f(fs[1].first, fs[1].second))
+    };
+    linii2[careLinie2][0] = line2[0];
+    linii2[careLinie2++][1] = line2[1];
+
+
+    sf::RenderWindow app2(sf::VideoMode(1400, 1050), "Press ESCAPE to Exit!");
+
+    while (app2.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (app2.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == sf::Event::Closed)
+                app2.close();
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+
+
+                    cout << "EXIT!";
+                    app2.close();
+                    return 0;
                 }
             }
         }
 
-        //Makes the background black.
-        window.clear(sf::Color::Black);
+        // Clear screen
+        app2.clear();
 
-
-        sf::ConvexShape conv;
-        conv.setFillColor(sf::Color::Black);
-        conv.setOutlineThickness(3);
-        conv.setOutlineColor(sf::Color::Green);
-        conv.setPointCount(point.size());
-        for (int i = 0; i < point.size(); ++i) {
-            Point p = point[i];
-            conv.setPoint(i, sf::Vector2f(p.x, p.y));
+        for(int i=0; i<n; i++){
+            sf::CircleShape cerc3(3);
+            cerc3.setPosition(a[i].first, a[i].second);
+            app2.draw(cerc3);
         }
 
-        //if (valid)
-        //{
-            window.draw(conv);
-        //}
 
-        for (const auto& r : point)
-        {
-            //Size of the circles.
-            sf::CircleShape shape(8);
+        if(ii<n){
+            if(j<n){
+                fi.push_back(a[ii]);
 
-            //Circle position.
-            shape.setPosition(r.x, r.y);
+                sf::Vertex line[]{
+                        sf::Vertex(sf::Vector2f(fi[ii - k - 1].first, fi[ii - k - 1].second)),
+                        sf::Vertex(sf::Vector2f(fi[ii - k].first, fi[ii - k].second))
+                };
+                linii[careLinie][0] = line[0];
+                linii[careLinie++][1] = line[1];
 
-            //Circle color.
-            shape.setFillColor(sf::Color::White);
+                while (fi.size() > 2)
+                {
+                    int x = orientare(fi[ii - 2 - k], fi[ii - k - 1], fi[ii - k]);
 
-            //Draw the shape.
-            window.draw(shape);
+                    if (x != 2)
+                    {
+                        careLinie -=2;
+                        sf::Vertex line[]{
+                                sf::Vertex(sf::Vector2f(fi[ii - 2 - k].first, fi[ii - 2 - k].second)),
+                                sf::Vertex(sf::Vector2f(fi[ii - k].first, fi[ii - k].second))
+                        };
+                        linii[careLinie][0] = line[0];
+                        linii[careLinie++][1] = line[1];
+
+                        fi[ii - k - 1] = fi[ii - k];
+
+                        fi.pop_back();
+                        k++;
+                    }
+                    else
+                        break;
+                }
+            }
         }
-        window.display();
+
+        ii++;
+
+        if(ii>=n){
+            if(fs_i >= 0){
+                fs.push_back(a[fs_i]);
+                sf::Vertex line[]{
+                        sf::Vertex(sf::Vector2f(fs[t+1-k2].first, fs[t+1-k2].second)),
+                        sf::Vertex(sf::Vector2f(fs[t+2-k2].first, fs[t+2-k2].second))
+                };
+                linii2[careLinie2][0] = line[0];
+                linii2[careLinie2++][1] = line[1];
+
+                while (fs.size() > 2)
+                {
+                    int x = orientare(fs[t-k2], fs[t+1-k2], fs[t+2-k2]);
+                    if (x != 2)
+                    {
+                        careLinie2 -=2;
+                        sf::Vertex line[]{
+                                sf::Vertex(sf::Vector2f(fs[t-k2].first, fs[t-k2].second)),
+                                sf::Vertex(sf::Vector2f(fs[t+2-k2].first, fs[t+2-k2].second))
+                        };
+                        linii2[careLinie2][0] = line[0];
+                        linii2[careLinie2++][1] = line[1];
+
+                        fs[t+1-k2] = fs[t+2-k2];
+                        fs.pop_back();
+                        k2++;
+                    }
+                    else
+                        break;
+                }
+                t++;
+            }
+            fs_i--;
+        }
+
+        for(int o = 0; o < careLinie; o++){
+            linii[o][0].color = sf::Color::Red;
+            linii[o][1].color = sf::Color::Red;
+            app2.draw(linii[o], 2, sf::Lines);
+        }
+        if(ii>=n)
+            for(int o = 0; o < careLinie2; o++){
+                linii2[o][0].color = sf::Color::Red;
+                linii2[o][1].color = sf::Color::Red;
+                app2.draw(linii2[o], 2, sf::Lines);
+            }
+
+        for(int i=0; i<fi.size(); i++){
+            sf::CircleShape cerc2(4);
+            cerc2.setFillColor(sf::Color(0, 255, 0));
+            cerc2.setPosition(fi[i].first, fi[i].second);
+            app2.draw(cerc2);
+        }
+
+        if(ii>=n)
+            for(int i=0; i<fs.size(); i++){
+                sf::CircleShape cerc2(4);
+                cerc2.setFillColor(sf::Color(0, 255, 0));
+                cerc2.setPosition(fs[i].first, fs[i].second);
+                app2.draw(cerc2);
+            }
+
+        j++;
+        // Update the window
+        app2.display();
+        //system("pause");
+
     }
 
-    return 0;
+
+    return EXIT_SUCCESS;
 }
